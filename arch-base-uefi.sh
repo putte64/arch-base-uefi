32d4f4a45c89a8b4c8583a2e48c8ca4c02035f48
putte64 <putte64@users.noreply.github.com>
2021-10-20 21:35:27 +0200

    Update arch-base-uefi.sh

--- arch-base-uefi.sh
+++ arch-base-uefi.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-hostname=
-localuser=
+hostname=bbk
+localuser=bbk
 
 
 ln -sf /usr/share/zoneinfo/Europe/Oslo /etc/localtime
 hwclock --systohc
-sed -i '177s/.//' /etc/locale.gen
+
+sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && \
+sed -i -e 's/# nb_NO.UTF-8 UTF-8/nb_NO.UTF-8 UTF-8/' /etc/locale.gen && \
+    
+# sed -i '177s/.//' /etc/locale.gen
 locale-gen
 echo "LANG=nb_NO.UTF-8" >> /etc/locale.conf
 echo "KEYMAP=no" >> /etc/vconsole.conf
@@ -24,26 +28,26 @@ reflector --latest 20 --protocol https --sort rate --save /etc/pacman.d/mirrorli
 pacman -Syyu 
 
 
-# pacman -Sy --needed - < pkglist.txt
-pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
+pacman -Sy --needed - < pkglist.txt
+# pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
 
 # pacman -S --noconfirm xf86-video-amdgpu
-# pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
-# pacman -S --noconfirm xf86-video-intel
+pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
+pacman -S --noconfirm xf86-video-intel
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
 systemctl enable NetworkManager
-systemctl enable bluetooth
+# systemctl enable bluetooth
 systemctl enable cups.service
 systemctl enable sshd
 systemctl enable avahi-daemon
-systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
-systemctl enable reflector.timer
+# systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
+# systemctl enable reflector.timer
 systemctl enable fstrim.timer
-systemctl enable libvirtd
-systemctl enable firewalld
+# systemctl enable libvirtd
+# systemctl enable firewalld
 systemctl enable acpid
 
 useradd -m $localuser
