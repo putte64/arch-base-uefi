8dc2b2fe05673ff50123bf8eaeb688d1b237f69d
putte64 <putte64@users.noreply.github.com>
2021-12-07 22:55:32 +0100

    Lots of updates

--- arch-base-uefi.sh
+++ arch-base-uefi.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
 # Add some custom variables here:
-hostname=bbk
-localuser=bbk
+hostname=test
+localuser=test
 
-# Change your zoneinfo if needed:
-ln -sf /usr/share/zoneinfo/Europe/Oslo /etc/localtime
+echo "-------------------------------------------------"
+echo "       Setup Language to NO and set locale       "
+echo "-------------------------------------------------"
+sed -i 's/^#nb_NO.UTF-8 UTF-8/nb_NO.UTF-8 UTF-8/' /etc/locale.gen
+locale-gen
+timedatectl --no-ask-password set-timezone Europe/Oslo
+timedatectl --no-ask-password set-ntp 1
+localectl --no-ask-password set-locale LANG="nb_NO.UTF-8" LC_TIME="nb_NO.UTF-8"
+localectl --no-ask-password set-keymap no
 hwclock --systohc
 
-# Method to set locale should e refined:
-# sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && \
-# sed -i -e 's/# nb_NO.UTF-8 UTF-8/nb_NO.UTF-8 UTF-8/' /etc/locale.gen && \  
-# sed -i '177s/.//' /etc/locale.gen
-
-# echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
-echo 'nb_NO.UTF-8 UTF-8' >> /etc/locale.gen
-
-locale-gen
-echo "LANG=nb_NO.UTF-8" >> /etc/locale.conf
 echo "KEYMAP=no" >> /etc/vconsole.conf
 echo "$hostname" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
@@ -28,18 +25,66 @@ echo root:password | chpasswd
 # You can add xorg to the installation packages, I usually add it at the DE or WM install script
 # You can remove the tlp package if you are installing on a desktop or vm
 
-# pacman -Syy
+################
+# Optimize mkpkg
+################
+
+nc=$(grep -c ^processor /proc/cpuinfo)
+echo "You have " $nc" cores."
+echo "-------------------------------------------------"
+echo "Changing the makeflags for "$nc" cores."
+TOTALMEM=$(cat /proc/meminfo | grep -i 'memtotal' | grep -o '[[:digit:]]*')
+if [[  $TOTALMEM -gt 8000000 ]]; then
+sed -i "s/#MAKEFLAGS=\"-j2\"/MAKEFLAGS=\"-j$nc\"/g" /etc/makepkg.conf
+echo "Changing the compression settings for "$nc" cores."
+sed -i "s/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T $nc -z -)/g" /etc/makepkg.conf
+fi
+
+######################################################################
+# Enable parallell download, install reflector, enable multilib and optimize mirrorlist
+######################################################################
+
 sed -i 's/^#Para/Para/' /etc/pacman.conf
 pacman -S --needed reflector rsync
 reflector --latest 20 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
+pacman -Sy --noconfirm
 pacman -Syyu 
 
 # See pkglist.txt for details and change to your needs
 pacman -Sy --needed - < pkglist.txt
 
-# pacman -S --noconfirm xf86-video-amdgpu
-pacman -S --noconfirm nvidia-lts nvidia-utils nvidia-settings
-pacman -S --noconfirm xf86-video-intel
+################################################
+# determine processor type and install microcode
+################################################
+proc_type=$(lscpu | awk '/Vendor ID:/ {print $3}')
+case "$proc_type" in
+	GenuineIntel)
+		print "Installing Intel microcode"
+		pacman -S --noconfirm intel-ucode
+		proc_ucode=intel-ucode.img
+		;;
+	AuthenticAMD)
+		print "Installing AMD microcode"
+		pacman -S --noconfirm amd-ucode
+		proc_ucode=amd-ucode.img
+		;;
+esac	
+
+###################################
+# Graphics Drivers find and install
+###################################
+
+if lspci | grep -E "NVIDIA|GeForce"; then
+    pacman -S nvidia-lts nvidia-utils nvidia-settings --noconfirm --needed
+	nvidia-xconfig
+elif lspci | grep -E "Radeon"; then
+    pacman -S xf86-video-amdgpu --noconfirm --needed
+elif lspci | grep -E "Integrated Graphics Controller"; then
+    pacman -S libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils --needed --noconfirm
+fi
+
+echo -e "\nDone!\n"
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
@@ -56,9 +101,22 @@ systemctl enable fstrim.timer
 # systemctl enable firewalld
 systemctl enable acpid
 
-useradd -m $localuser
-echo $localuser:password | chpasswd
-usermod -aG libvirt wheel $localuser
+if ! source install.conf; then
+	read -p "Please enter username:" username
+echo "username=$username" >> ${HOME}/ArchTitus/install.conf
+fi
+if [ $(whoami) = "root"  ];
+then
+    useradd -m -G wheel,libvirt -s /bin/bash $username 
+	passwd $username
+	cp -R /root/ArchTitus /home/$username/
+    chown -R $username: /home/$username/ArchTitus
+	read -p "Please name your machine:" nameofmachine
+	echo $nameofmachine > /etc/hostname
+else
+	echo "You are already a user proceed with aur installs"
+fi
+
 
 # Is this a viable method????
 # echo "$localuser ALL=(ALL) ALL" >> /etc/sudoers.d/$localuser
