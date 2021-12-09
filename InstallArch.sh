773e9ac40c8b344d33a7d71f718422874893a1b3
putte64 <putte64@users.noreply.github.com>
2021-12-09 22:26:14 +0100

    Update InstallArch.sh

--- InstallArch.sh
+++ InstallArch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
     bash 0-prepare.sh
-    #arch-chroot /mnt /root/arch-base-uefi/1-arch-base-uefi.sh
-    #source /mnt/root/arch-base-uefi/install.conf
+    arch-chroot /mnt /root/arch-base-uefi/1-arch-base-uefi.sh
+    source /mnt/root/arch-base-uefi/install.conf
     #arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/arch-base-uefi/2-user.sh
     #arch-chroot /mnt /root/arch-base-uefi/3-finalize.sh
     echo "###################################################################################"
