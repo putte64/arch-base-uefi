8edc6897d157841537dce2d4ce738aad14bbaad0
putte64 <putte64@users.noreply.github.com>
2021-12-09 19:43:09 +0100

    Troubleshooting partitioning

--- 0-prepare.sh
+++ 0-prepare.sh
@@ -9,9 +9,9 @@ echo "-------------------------------------------------"
 iso=$(curl -4 ifconfig.co/country-iso)
 timedatectl set-ntp true
 pacman -S --noconfirm pacman-contrib terminus-font
-#setfont ter-v12b
+setfont ter-112n
 sed -i 's/^#Para/Para/' /etc/pacman.conf
-pacman -S --noconfirm reflector rsync grub
+pacman -S --noconfirm rsync grub
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 
 echo -e "-------------------------------------------------------------------------"
@@ -19,11 +19,11 @@ echo -e "-Setting up $iso mirrors for faster downloads"
 echo -e "-------------------------------------------------------------------------"
 
 reflector -a 12 -c $iso,se,dk,nl,de -f 5 -l 20 --sort rate --sort country -p https --threads 2 --save /etc/pacman.d/mirrorlist
-mkdir /mnt
+#############################mkdir /mnt
 
 
 echo -e "\nInstalling prereqs...\n$HR"
-pacman -S --noconfirm gptfdisk
+###################pacman -S --noconfirm gptfdisk
 
 
 echo "-------------------------------------------------"
@@ -46,10 +46,10 @@ sgdisk -Z ${DISK} # zap all on disk
 sgdisk -a 2048 -o ${DISK} # new gpt disk 2048 alignment
 
 # create partitions
-sgdisk -n 1::+512M --typecode=1:ef00 --change-name=1:'EFI' ${DISK} # partition 1 (EFI Partition)
-sgdisk -n 2::+4G --typecode=2:8200 --change-name=2:'SWAP' ${DISK} # partition 2 (SWAP Partition)
-sgdisk -n 3::+50G --typecode=3:8300 --change-name=3:'ROOT' ${DISK} # partition 3 (ROOT Partition)
-sgdisk -n 4::-0 --typecode=4:8300 --change-name=4:'HOME' ${DISK} # partition 4 (HOME Partition), default start, remaining-128M
+sgdisk -n 1::+300M --typecode=0:ef00 --change-name=1:'EFI' ${DISK} # partition 1 (EFI Partition)
+sgdisk -n 2::+2G --typecode=1:8200 --change-name=2:'SWAP' ${DISK} # partition 2 (SWAP Partition)
+sgdisk -n 3::+30G --typecode=2:8300 --change-name=3:'ROOT' ${DISK} # partition 3 (ROOT Partition)
+sgdisk -n 4::-0 --typecode=3:8300 --change-name=4:'HOME' ${DISK} # partition 4 (HOME Partition), default start, remaining-128M
 if [[ ! -d "/sys/firmware/efi" ]]; then
     sgdisk -A 1:set:2 ${DISK}
 fi
