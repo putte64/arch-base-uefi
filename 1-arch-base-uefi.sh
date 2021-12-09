60ed1d7781c13e6e93b31d50c5ff303318913dbe
putte64 <putte64@users.noreply.github.com>
2021-12-09 22:24:21 +0100

    Update 1-arch-base-uefi.sh

--- 1-arch-base-uefi.sh
+++ 1-arch-base-uefi.sh
@@ -1,33 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Add some custom variables here:
-hostname=test
-localuser=test
-
+echo "--------------------------------------"
+echo "--          Network Setup           --"
+echo "--------------------------------------"
+pacman -S networkmanager dhclient --noconfirm --needed
+systemctl enable --now NetworkManager
 echo "-------------------------------------------------"
-echo "       Setup Language to NO and set locale       "
+echo "Setting up mirrors for optimal download          "
 echo "-------------------------------------------------"
-sed -i 's/^#nb_NO.UTF-8 UTF-8/nb_NO.UTF-8 UTF-8/' /etc/locale.gen
-locale-gen
-timedatectl --no-ask-password set-timezone Europe/Oslo
-timedatectl --no-ask-password set-ntp 1
-localectl --no-ask-password set-locale LANG="nb_NO.UTF-8" LC_TIME="nb_NO.UTF-8"
-localectl --no-ask-password set-keymap no
-hwclock --systohc
-
-echo "KEYMAP=no" >> /etc/vconsole.conf
-echo "$hostname" >> /etc/hostname
-echo "127.0.0.1 localhost" >> /etc/hosts
-echo "::1       localhost" >> /etc/hosts
-echo "127.0.1.1 $hostname.localdomain $hostname" >> /etc/hosts
-echo root:password | chpasswd
-
-# You can add xorg to the installation packages, I usually add it at the DE or WM install script
-# You can remove the tlp package if you are installing on a desktop or vm
-
-################
-# Optimize mkpkg
-################
+pacman -S --noconfirm --needed pacman-contrib curl
+pacman -S --noconfirm --needed reflector rsync
+cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
 
 nc=$(grep -c ^processor /proc/cpuinfo)
 echo "You have " $nc" cores."
@@ -39,24 +22,39 @@ sed -i "s/#MAKEFLAGS=\"-j2\"/MAKEFLAGS=\"-j$nc\"/g" /etc/makepkg.conf
 echo "Changing the compression settings for "$nc" cores."
 sed -i "s/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T $nc -z -)/g" /etc/makepkg.conf
 fi
+echo "-------------------------------------------------"
+echo "       Setup Language to NO and set locale       "
+echo "-------------------------------------------------"
+sed -i 's/^#nb_NO.UTF-8 UTF-8/nb_NO.UTF-8 UTF-8/' /etc/locale.gen
+locale-gen
+timedatectl --no-ask-password set-timezone Europe/Oslo
+timedatectl --no-ask-password set-ntp 1
+hwclock --systohc
+localectl --no-ask-password set-locale LANG="nb_NO.UTF-8" LC_TIME="nb_NO.UTF-8"
 
-######################################################################
-# Enable parallell download, install reflector, enable multilib and optimize mirrorlist
-######################################################################
+# Set keymaps
+localectl --no-ask-password set-keymap no
 
+# Add sudo no password rights
+sed -i 's/^# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
+
+#Add parallel downloading
 sed -i 's/^#Para/Para/' /etc/pacman.conf
-pacman -S --needed reflector rsync
-reflector --latest 20 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+
+#Enable multilib
 sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
 pacman -Sy --noconfirm
-pacman -Syyu 
+
+echo -e "\nInstalling Base System\n"
+
 
 # See pkglist.txt for details and change to your needs
 pacman -Sy --needed --noconfirm - < pkglist.txt
 
-################################################
+
+#
 # determine processor type and install microcode
-################################################
+# 
 proc_type=$(lscpu | awk '/Vendor ID:/ {print $3}')
 case "$proc_type" in
 	GenuineIntel)
@@ -71,12 +69,9 @@ case "$proc_type" in
 		;;
 esac	
 
-###################################
 # Graphics Drivers find and install
-###################################
-
 if lspci | grep -E "NVIDIA|GeForce"; then
-    pacman -S nvidia-lts nvidia-utils nvidia-settings --noconfirm --needed
+    pacman -S nvidia --noconfirm --needed
 	nvidia-xconfig
 elif lspci | grep -E "Radeon"; then
     pacman -S xf86-video-amdgpu --noconfirm --needed
@@ -85,50 +80,18 @@ elif lspci | grep -E "Integrated Graphics Controller"; then
 fi
 
 echo -e "\nDone!\n"
-
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
-grub-mkconfig -o /boot/grub/grub.cfg
-
-systemctl enable NetworkManager
-# systemctl enable bluetooth
-systemctl enable cups
-systemctl enable sshd
-systemctl enable avahi-daemon
-# systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
-# systemctl enable reflector.timer
-systemctl enable fstrim.timer
-# systemctl enable libvirtd
-# systemctl enable firewalld
-systemctl enable acpid
-
-echo -e "\nEnabling Login Display Manager"
-systemctl enable sddm.service
-echo -e "\nSetup SDDM Theme"
-cat <<EOF > /etc/sddm.conf
-[Theme]
-Current=Nordic
-EOF
-
 if ! source install.conf; then
 	read -p "Please enter username:" username
-echo "username=$username" >> /arch-base-uefi/install.conf
+echo "username=$username" >> ${HOME}/arch-base-uefi/install.conf
 fi
 if [ $(whoami) = "root"  ];
 then
     useradd -m -G wheel,libvirt -s /bin/bash $username 
 	passwd $username
-	cp -R /root/ArchTitus /home/$username/
+	cp -R /root/arch-base-uefi /home/$username/
     chown -R $username: /home/$username/arch-base-uefi
 	read -p "Please name your machine:" nameofmachine
 	echo $nameofmachine > /etc/hostname
 else
 	echo "You are already a user proceed with aur installs"
 fi
-
-
-# Is this a viable method????
-# echo "$localuser ALL=(ALL) ALL" >> /etc/sudoers.d/$localuser
-
-
-printf "\e[1;32mDone! Type exit, umount -R /mnt and reboot.\e[0m"
-
