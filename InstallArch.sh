6e347c33eb23a46218d5fa7804d28f8048514a93
putte64 <putte64@users.noreply.github.com>
2022-01-27 20:51:11 +0100

    Update InstallArch.sh

--- InstallArch.sh
+++ InstallArch.sh
@@ -3,13 +3,19 @@
         exec 5> debug_output.txt
         BASH_XTRACEFD="5"
         PS4='$LINENO: '
-        set -ex
-
-    bash 0-prepare.sh
-    arch-chroot /mnt /root/arch-base-uefi/1-arch-base-uefi.sh
-    source /mnt/root/arch-base-uefi/install.conf
-    arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/arch-base-uefi/2-user.sh
-    arch-chroot /mnt /root/arch-base-uefi/3-finalize.sh
+        set -e
+        
+# Find the name of the folder the scripts are in
+        setfont ter-v22b
+        SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+        
+    bash startup.sh
+    source $SCRIPT_DIR/setup.conf
+    bash 0-preinstall.sh
+    arch-chroot /mnt /root/arch-base-uefi/1-setup.sh
+    arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- /home/$USERNAME/arch-base-uefi/2-user.sh
+    arch-chroot /mnt /root/arch-base-uefi/3-post-setup.sh
+    
     echo "###################################################################################"
     echo "###################################################################################"
     echo "  The End The End The End The End The End The End The End The End The End The End  "
