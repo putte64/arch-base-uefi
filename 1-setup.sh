48ac634bcde0700dcf98f85f1cbdf813b90e7ea5
putte64 <putte64@users.noreply.github.com>
2022-02-05 22:08:05 +0100

    Add test after "Adding user"

--- 1-setup.sh
+++ 1-setup.sh
@@ -116,8 +116,6 @@ esac
 echo "processor install ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤"
 echo $proc_type
 
-sleep 10
-
 echo -ne "
 -------------------------------------------------------------------------
                     Installing Graphics Drivers
@@ -140,6 +138,48 @@ echo -ne "
                     Adding User
 -------------------------------------------------------------------------
 "
+
+#SETUP IS WRONG THIS IS RUN
+if ! source /root/ArchTitus/setup.conf; then
+	# Loop through user input until the user gives a valid username
+	while true
+	do 
+		read -p "Please enter username:" username
+		# username regex per response here https://unix.stackexchange.com/questions/157426/what-is-the-regex-to-validate-linux-users
+		# lowercase the username to test regex
+		if [[ "${username,,}" =~ ^[a-z_]([a-z0-9_-]{0,31}|[a-z0-9_-]{0,30}\$)$ ]]
+		then 
+			break
+		fi 
+		echo "Incorrect username."
+	done 
+# convert name to lowercase before saving to setup.conf
+echo "username=${username,,}" >> ${HOME}/ArchTitus/setup.conf
+
+    #Set Password
+    read -p "Please enter password:" password
+echo "password=${password,,}" >> ${HOME}/ArchTitus/setup.conf
+
+    # Loop through user input until the user gives a valid hostname, but allow the user to force save 
+	while true
+	do 
+		read -p "Please name your machine:" nameofmachine
+		# hostname regex (!!couldn't find spec for computer name!!)
+		if [[ "${nameofmachine,,}" =~ ^[a-z][a-z0-9_.-]{0,62}[a-z0-9]$ ]]
+		then 
+			break 
+		fi 
+		# if validation fails allow the user to force saving of the hostname
+		read -p "Hostname doesn't seem correct. Do you still want to save it? (y/n)" force 
+		if [[ "${force,,}" = "y" ]]
+		then 
+			break 
+		fi 
+	done 
+
+    echo "nameofmachine=${nameofmachine,,}" >> ${HOME}/ArchTitus/setup.conf
+fi
+
 
 sleep 10
 
