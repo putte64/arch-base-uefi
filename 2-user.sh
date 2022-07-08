407ace661de3a56a3cd55448aa64fd7df160404d
putte64 <putte64@users.noreply.github.com>
2022-07-08 17:29:48 +0200

    Comment out ocs-url

--- 2-user.sh
+++ 2-user.sh
@@ -33,7 +33,7 @@ PKGS=(
 'noto-fonts-emoji'
 'papirus-icon-theme'
 'plasma-pa'
-'ocs-url' # install packages from websites
+# 'ocs-url' # install packages from websites
 'sddm-nordic-theme-git'
 'ttf-droid'
 'ttf-hack'
