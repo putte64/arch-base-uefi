d931c75cf5c1164ea27474602801d47cf7187e0a
putte64 <putte64@users.noreply.github.com>
2022-07-08 18:54:38 +0200

    change to " on echo

--- 1-setup.sh
+++ 1-setup.sh
@@ -60,7 +60,7 @@ localectl --no-ask-password set-locale LANG="nb_NO.UTF-8" LC_TIME="nb_NO.UTF-8"
 
 # Set keymaps
 # localectl --no-ask-password set-keymap ${KEYMAP}
-echo 'KEYMAP=no' >> /etc/vconsole.conf
+echo "KEYMAP=no" >> /etc/vconsole.conf
 
 sleep 10
 
