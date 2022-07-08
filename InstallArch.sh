f4d5ab2c9c3ade1b72f3d749e059597c97d996af
putte64 <putte64@users.noreply.github.com>
2022-07-08 13:56:25 +0200

    Add log to scripts

--- InstallArch.sh
+++ InstallArch.sh
@@ -6,12 +6,13 @@ set -eu -o pipefail
         setfont ter-v12n
         SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-    bash startup.sh
+    ( bash startup.sh )|& tee startup.log
     source $SCRIPT_DIR/install.conf
-    bash 0-preinstall.sh
-    arch-chroot /mnt /root/arch-base-uefi/1-setup.sh
-    arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- /home/$USERNAME/arch-base-uefi/2-user.sh
-    arch-chroot /mnt /root/arch-base-uefi/3-post-setup.sh
+    ( bash 0-preinstall.sh )|& tee 0-preinstall.log
+    ( arch-chroot /mnt /root/arch-base-uefi/1-setup.sh )|& tee 1-setup.log
+    ( arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- /home/$USERNAME/arch-base-uefi/2-user.sh )|& tee 2-user.log
+    ( arch-chroot /mnt /root/arch-base-uefi/3-post-setup.sh )|& tee 3-post-setup.log
+    cp -v *.log /mnt/home/$USERNAME
     
     echo "###################################################################################"
     echo "###################################################################################"
