e5096ba55125a6c85ede55362a13b251135339cc
putte64 <putte64@users.noreply.github.com>
2022-10-05 20:47:45 +0200

    Unmount all disk before formating disk

--- 0-preinstall.sh
+++ 0-preinstall.sh
@@ -44,6 +44,8 @@ echo -ne "
                     Formating Disk
 -------------------------------------------------------------------------
 "
+umount -A --recursive /mnt # make sure everything is unmounted before we start
+
 sleep 5
 # disk prep
 sgdisk -Z ${DISK} # zap all on disk
